0f899ccb4c100e2f56c8aecd6f9ca3ca150b9778
fiftie <kumavichi.t@gmail.com>
2019-08-08 15:48:47 +0900

    Initial Commit

---
+++ OmikujiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OmikujiApp
+//
+//  Created by 五十嵐龍吉 on 2019/08/08.
+//  Copyright © 2019 Tatsuyoshi Igarashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
